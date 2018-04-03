2706dc3eb81d851cade9e85a0880a9998fce7b61
Igor Lucas <igor.lucas@nostrum.com.br>
2018-04-03 09:27:42 -0300

    fix angular post

--- src/main/resources/public/js/app.js
+++ src/main/resources/public/js/app.js
@@ -2,14 +2,25 @@ var app = angular.module('myApp', []);
 app.controller('FormCtrl', function ($scope, $http) {
     $scope.submitForm = function() {
         console.log("posting data....");
-        $http.post('/public/user/add', JSON.stringify($scope.formData))
-            .then(function(success) {
-                if (success) {
-                    $scope.message = 'OK';
-                } else {
-                    $scope.message = 'ERROR';
-                }
-            }).catch(function (err) {
+
+        var req = {
+            method: 'POST',
+            url: '/public/user/add',
+            headers: {
+              'Content-Type': 'application/json'
+            },
+            data: {
+                "user" : $scope.formData
+            }
+        };
+
+        $http(req)
+            .then(function successCallback(response) {
+                $scope.message = 'OK';
+            }, function errorCallback(response) {
+                $scope.message = 'ERROR';
+            })
+            .catch(function (err) {
                 console.log(err);
             });
     };
